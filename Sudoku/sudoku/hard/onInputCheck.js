3b5e8d0aa3b8499da658a0193334615939ad40d2
SargisGalstyan <94855634+SargisGalstyan@users.noreply.github.com>
2022-02-21 23:47:10 +0400

    Update onInputCheck.js

--- Sudoku/sudoku/hard/onInputCheck.js
+++ Sudoku/sudoku/hard/onInputCheck.js
@@ -1,5 +1,4 @@
-// let wrapper = document.querySelector('.board');
-// let check = document.querySelector('.check');
+
 let hard = document.querySelector('.hard');
 let checkHard = document.querySelector('.addRandom');
 function checkNum(event) {
@@ -8,13 +7,11 @@ function checkNum(event) {
   if (!event.target.innerHTML.match(onlyNumbers) && event.target.innerHTML) {
     event.target.style.backgroundColor = '#e65022';
     checkHard.setAttribute('disabled', 'disabled');
-    // check.setAttribute('disabled', 'disabled');
   } else {
-    event.target.style.backgroundColor = 'initial';
     checkHard.removeAttribute('disabled', 'disabled');
     // check.removeAttribute('disabled', 'disabled');
   }
 }
 
 hard.addEventListener('input', checkNum);
-// wrapper.addEventListener('input', checkNum);
+
